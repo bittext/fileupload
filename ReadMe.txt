779465e4604887b4981a64b259717f5000b7f8b1
spulic <satishpulickal@gmail.com>
2017-08-14 11:33:34 -0500

    fileupload

---
+++ ReadMe.txt
@@ -0,0 +1,11 @@
+ReadMe
+=============
+
+Rest API to upload a file with few meta data information stored to a H2 database and the file saved to a file system
+
+#For compiling run command
+ > mvn spring-boot:run
+
+#For running the application 
+   url > http://localhost:8080/
+
--- src/test/java/com/example/fileupload/controller/FileUploadControllerTests.java
+++ src/test/java/com/example/fileupload/controller/FileUploadControllerTests.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -27,7 +28,9 @@ public class FileUploadControllerTests {
 
     @Test
     public void shouldReturnWithNoErrors() throws Exception {
-        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+    	MockMultipartFile multipartFile = new MockMultipartFile("file", "test.txt",
+                "text/plain", "Text content for testing".getBytes());
+        this.mockMvc.perform(get("/",multipartFile)).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("Success")));
     }
 }
